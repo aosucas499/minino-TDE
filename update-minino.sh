9bad4618e3447d596137406ca37d67139a45744a
aosucas499 <aosucas499@gmail.com>
2020-12-04 21:20:14 +0100

    Añadido paquete git

--- update-minino.sh
+++ update-minino.sh
@@ -4,3 +4,9 @@
 
 #Menú gráficos duplicado en ImageMagik-corregido
 sudo rm /usr/share/applications/display-im6.q16.desktop
+
+# Añadir paquete "git" para descargar directamente al sistema desde Github.
+sudo apt-get install git -y
+
+# Instalar programa GIMP 
+#sudo apt update && sudo apt-get install gimp -y
