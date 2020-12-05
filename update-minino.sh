269e418214e0b6b3c3477143532fff08fcdc03b5
Jose Antonio <jasvazquez@gmail.com>
2020-12-05 19:47:11 +0100

    Añadimos menú de personalización

--- update-minino.sh
+++ update-minino.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+# =============================================================================
 # Script para ejecutar en la iso y añadir mejoras.
+# =============================================================================
 
-#Menú gráficos duplicado en ImageMagik-corregido
-sudo rm /usr/share/applications/display-im6.q16.desktop
+# -----------------------------------------------------------------------------
+# Definición de las funciones utilizadas en el script
+# -----------------------------------------------------------------------------
 
-# Añadir paquete "git" para descargar directamente al sistema desde Github.
-sudo apt update && sudo apt-get install git -y
+# Instala Gimp en el sistema
+# ---
 
-# Instalar programa GIMP 
-sudo apt-get install gimp -y
+function instalarGimp {
+    sudo apt-get install gimp -y
+}
 
-# Autologin para usuario "usuario"
+# Instala GIT en el sistema
+# ---
+
+function instalarGit {
+
+    # Añadir paquete "git" para descargar directamente al sistema desde Github.
+    sudo apt update && sudo apt-get install git -y
+}
+
+# Activa el autologin para el usuario "usuario"
+# ---
+
+function activarAutoLogin {
 
 cat << EOF >> /etc/lightdm/lightdm.conf 
 
@@ -25,3 +41,82 @@ greeter-session=lightdm-greeter
 
 EOF
 
+}
+
+# Corrige la opción de menú duplicidad para ImageMagick
+# ---
+
+function corregirImageMagick {
+
+    # Menú gráficos duplicados en ImageMagik-corregido
+    sudo rm /usr/share/applications/display-im6.q16.desktop
+}
+
+# Ejecuta la función correspondiente a cada una de las opciones del script
+# ---
+
+function ejecutarAccionOpcional {
+
+    echo "Ejecutamos $1()"
+    ($1)
+}
+
+# Invocamos ("callback") las funciones asociadas a las opciones 
+# seleccionadas por el usuario
+# ---
+
+function procesarAccionesOpcionales {
+
+    # Dividimos (el separador es "|" ) las opciones seleccionadas por el usuario
+    # ---
+
+    IFS="|" read -a vals <<< $1
+
+    # Solicitamos (una a una) que se procesen dichas opciones
+
+    for i in "${vals[@]}"
+    do
+        ejecutarAccionOpcional $i
+    done
+
+}
+
+# -----------------------------------------------------------------------------
+# Cuerpo del script
+# -----------------------------------------------------------------------------
+
+# Realizamos las opciones por defecto de nuestro script
+# ---
+
+instalarGit
+corregirImageMagick
+
+# Permitimos seleccionar opciones personalizadas
+# ---
+
+# Mostramos las opciones personalizables
+
+opc=$( \
+    zenity \
+        --list \
+        --title="Elija las personalizaciones que desea apliar" \
+        --checklist \
+        --column="Aplicar" \
+        --column="funcionAEjecutar" \
+        --column="Descripción" \
+        --hide-column=2 \
+    True activarAutoLogin "Inicio de sesión automático" \
+    False instalarGimp "Instalar Gimp" \
+)
+
+# Comprobamos que no se pulse el botón Cancelar
+
+if [[ "$?" != 0 ]]; then
+    echo "Sin problemas, ya personalizaremos Minino otro día ;)"
+    exit 0
+fi
+
+# Procesamos las opciones elegidas por el usuario
+# ---
+
+procesarAccionesOpcionales $opc
