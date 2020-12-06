4b3de33911bf49fd4f5ecc459e352121a92f0ff3
aosucas499 <aosucas499@gmail.com>
2020-12-06 14:00:06 +0100

    Delete test-ntp.sh

--- test-ntp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dpkg-reconfigure tzdata
-sudo cp ./ntp/fix-ntp /usr/bin
-sudo chmod +x /usr/bin/fix-ntp
-sudo chown root:root ./ntp/zz-fix-ntp
-sudo chmod 0440 ./ntp/zz-fix-ntp
-sudo cp ./ntp/zz-fix-ntp /etc/sudoers.d/ 
-sudo cp ./ntp/fix-ntp.desktop /etc/xdg/autostart/
