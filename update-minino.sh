fd139437e585b72ba99729adc67e8da434a12252
aosucas499 <aosucas499@gmail.com>
2021-01-07 16:18:37 +0100

    delete system back, add refracta & get ready ISO

--- update-minino.sh
+++ update-minino.sh
@@ -90,7 +90,7 @@ function customize-app {
 function firefox83-system {
 
 	# Eliminamos del sistema la version noroot para el usuario
-    sudo rm -r /home/$USER/firefox
+    	sudo rm -r /home/$USER/firefox
 	sudo rm /home/$USER/Escritorio/Firefox-83
 	sudo rm /home/$USER/.local/share/applications/firefox-noroot.desktop
 	sudo rm -r /home/$USER/Descargas/actualiza-firefox-guadalinex-master
@@ -100,7 +100,7 @@ function firefox83-system {
   	# Instala firefox 83 en el sistema
 	echo -e "${AZUL}Descargando Firefox para arquitecturas de 32 bits${NORMAL}"
 	wget $FIREFOX -q --show-progress
-    echo -e "${AZUL}Firefox se está descomprimiendo en un directorio del sistema...${NORMAL}"
+    	echo -e "${AZUL}Firefox se está descomprimiendo en un directorio del sistema...${NORMAL}"
 	sudo tar -xjf firefox*.tar.bz2 -C /usr/lib
 	sudo mv /usr/lib/firefox /usr/lib/firefox-latest
 	echo -e "${AZUL}Creando accesos directos...${NORMAL}"
@@ -118,6 +118,49 @@ function firefox83-system {
 
     
 }
+
+function prepareIso {
+	echo -e "${AZUL}Preparando la ISO${NORMAL}"
+	#Borramos archivos innecesarios 
+	sudo rm ~/.local/share/applications/appimagekit-balena-etcher-electron.desktop
+	sudo rm -r /home/usuario/Descargas/*
+	sudo rm -r /home/Systemback
+
+	#Instalamos refracta que desinstala systemback
+	wget https://sourceforge.net/projects/refracta/files/tools/refractasnapshot-base_10.2.10_all.deb
+	sudo dpkg -i refractasnapshot*
+	sudo apt-get install -f -y
+
+	#modificamos opciones y texto del grub
+	sudo cp ./tools/splash.png /usr/lib/refractasnapshot/iso/isolinux/splash.png
+	sudo cp ./tools/splash.png /usr/lib/refractasnapshot/iso/isolinux-refracta/splash.png
+	sudo sed -i "s/GALPon/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/isolinux.conf"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_ca.cfg"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_ca.conf"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_en.cfg"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_en.conf"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_es.cfg"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_es.conf"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_eu.cfg"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_eu.conf"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_gl.cfg"
+	sudo sed -i "s/Queiles/#TDE/g" "/usr/lib/refractasnapshot/iso/isolinux/menu_gl.conf"
+
+	#modificaciones del instalador
+	sudo sed -i "s/minino/usuario/g" "/usr/local/bin/minino-installer-selector"
+	sudo sed -i -e '311,312 s/#//g' /usr/local/sbin/makelive
+	sudo sed -i -e '15,17 s/#//g' /usr/lib/refractainstaller/post-install/cleanup-install.sh
+	sudo sed -i '15,17 s/refractainstaller/minino-installer/g' /usr/lib/refractainstaller/post-install/cleanup-install.sh
+	sudo apt-get update -y
+
+	#borrar repo git
+	cd ~ && sudo rm -r minino*
+
+	echo -e "${AZUL}comandos para generar ISO{NORMAL}"
+	echo "prev-mklive"
+	echo "sudo makelive"
+}
+
 
 # -----------------------------------------------------------------------------
 # Cuerpo del script
@@ -139,3 +182,4 @@ corregirImageMagick
 customize-app
 firefox83-system
 showAsterisks
+prepareIso
