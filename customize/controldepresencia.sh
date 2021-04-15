0a94e10eeaf8b903f2e70c122d8e4b244ae40760
aosucas499 <aosucas499@gmail.com>
2021-04-15 19:42:01 +0200

    Add files via upload

--- customize/controldepresencia.sh
+++ customize/controldepresencia.sh
@@ -2,7 +2,8 @@
 
 function controlPresencia {
     sed -i '$ i\user_pref("browser.startup.homepage", "https://seneca.juntadeandalucia.es/controldepresencia/");' ~/.mozilla/firefox/*.default-release-1/prefs.js
-
+	wget https://raw.githubusercontent.com/aosucas499/minino-TDE/main/tools/Firefox-latest-sleep30
+    sudo cp Firefox-latest-sleep30 /etc/xdg/autostart/Firefox-latest-sleep30.desktop
     
 }
 
