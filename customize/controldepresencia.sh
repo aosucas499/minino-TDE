59d1524a0abbff70ba923b8e3dc9fd9651719f87
aosucas499 <aosucas499@gmail.com>
2021-04-21 00:18:36 +0200

    Update controldepresencia.sh

--- customize/controldepresencia.sh
+++ customize/controldepresencia.sh
@@ -15,10 +15,10 @@ function controlPresencia {
 	
 	# Borramos del archivo de configuración de firefox la pantalla de bienvenida que se ejecuta la primera vez
 	# Se hace añadiendo esta línea al archivo de configuración
-	sed -i '$ i\user_pref("browser.startup.homepage", "https://seneca.juntadeandalucia.es/controldepresencia/");' ~/.mozilla/firefox/*.default-release-1/prefs.js
+	sed -i '$ i\user_pref("trailhead.firstrun.didSeeAboutWelcome", true);' ~/.mozilla/firefox/*.default-release-1/prefs.js
 	
     	# Establecemos como página principal la web del control de presencia de Séneca 
-    	sed -i '$ i\user_pref("trailhead.firstrun.didSeeAboutWelcome", true);' ~/.mozilla/firefox/*.default-release-1/prefs.js
+    	sed -i '$ i\user_pref("browser.startup.homepage", "https://seneca.juntadeandalucia.es/controldepresencia/");' ~/.mozilla/firefox/*.default-release-1/prefs.js
 	
 	# Descargamos y copiamos el ejecutable de firefox en modo kiosk que se ejecutará en cada inicio  
 	# con un retardo para que le dé tiempo al script ntp de corregir la hora
