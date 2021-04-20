711ba7c40e44460df663152464a23408c1882844
aosucas499 <aosucas499@gmail.com>
2021-04-20 23:07:41 +0200

    Delete test.sh

--- customize/test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-firefoxConfigFile=/home/$USER/.mozilla/firefox/*1/prefs.js
-
-if [ -d "$firefoxConfigFile" ]; then
-	echo "existe $firefoxConfigFile"
-else	
-	echo "no existe $firefoxConfigFile"
-fi
