f937435031f21ec0af5185745d6501f03ca5f5f3
aosucas499 <aosucas499@gmail.com>
2021-05-30 22:53:07 +0200

    3sec wait to check connection at boot

--- update-minino.sh
+++ update-minino.sh
@@ -537,6 +537,7 @@ hasSudoRights(){
 # -----------------------------------------------------------------------------
 
 function isConnectionAvailable {
+	sleep 3
     echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1 && echo "True" || echo "False"
 }
 
