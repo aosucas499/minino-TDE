d61cb0b2534bab25a103ca6f01407ecb843a4861
aosucas499 <aosucas499@gmail.com>
2023-06-22 22:30:09 +0000

    fix repos with update-minino

--- update-minino.sh
+++ update-minino.sh
@@ -12,11 +12,16 @@
 # pruebas sin que el cambio de "release" afecte a los usuarios que ya tenga
 # autoupdate en Minino
 
-REPO_GITHUB=aosucas499/minino-TDE
+REPO_GITHUB=aosucas499/minino-testing
 
 FIREFOX=https://download-installer.cdn.mozilla.net/pub/firefox/releases/83.0/linux-i686/es-ES/firefox-83.0.tar.bz2
 LANZADOR=https://raw.githubusercontent.com/aosucas499/actualiza-firefox/master/firefox-latest.desktop
 NEWLANZADOR=firefox-latest.desktop
+
+source=/etc/apt/sources.list
+source2=/etc/apt/sources.list.d/multimedia2.list
+sourceSize=$(du -b $source|cut -f 1)
+source2Size=$(du -b $source2|cut -f 1)
 
 ROJO="\033[1;31m"
 NORMAL="\033[0m"
@@ -380,6 +385,34 @@ fi
 
 }
 
+function fixSource {
+
+	
+	if [[ $sourceSize = "748" ]]; then
+		
+		echo -e "${AZUL}Fichero sources.list apropiado${NORMAL}"
+	else
+		sudo wget https://raw.githubusercontent.com/aosucas499/sources/main/minino-tde-actual.list -O /tmp/sources.list
+		sudo cp /tmp/sources.list $source
+		sudo apt update -y
+		echo -e "${AZUL}Fichero sources.list corregido${NORMAL}"
+	fi
+}
+
+function fixSource2 {
+
+	
+	if [[ $source2Size = "60" ]]; then
+		
+		echo -e "${AZUL}Fichero sources2.list apropiado${NORMAL}"
+	else
+		sudo wget https://raw.githubusercontent.com/aosucas499/sources/main/minino-tde-multimedia-actual.list -O /tmp/multimedia2.list
+		sudo cp /tmp/multimedia2.list $source2
+		sudo apt update -y
+		echo -e "${AZUL}Fichero multimedia2.list corregido${NORMAL}"
+	fi
+}
+
 function prepareIso {
 	
 	echo -e "${AZUL}Preparando la ISO${NORMAL}"
@@ -649,6 +682,8 @@ customize-app
 firefox83-system
 sudoersUpdate
 fixmultimediaSource
+fixSource
+fixSource2
 
 autostartUpdateMinino
 
